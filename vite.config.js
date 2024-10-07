1206759f202cbbc167440bf7b47fe91cf398e6d3
Harshad0600 <hpasha2140@gmail.com>
2024-10-07 22:01:44 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Digital_clock/",
+  base:"/Digital_Clock/",
   plugins: [react()],
 })
